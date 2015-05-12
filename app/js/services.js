a86795220ef6e84f030cc00fc74b4b44c4e64e66
Victor Logunov <v.logunov@advmaker.net>
2015-05-12 19:01:20 +0300

    interim commit

--- app/js/services.js
+++ app/js/services.js
@@ -3,3 +3,17 @@ var twitServices = angular.module('twitServices', ['ngResource']);
 twitServices.factory('Tweet', ['$resource', function($resource) {
     return $resource('/api/tweets/:id');
 }]);
+
+twitServices.factory('TweetAuth', ['$resource', function($resource) {
+    return $resource('https://api.twitter.com/oauth2/token', {}, {
+        get: {
+            method: 'POST',
+            params: {
+                grant_type: 'client_credentials',
+                headers: {
+                    Authorization: ''
+                }
+            }
+        }
+    })
+}]);
